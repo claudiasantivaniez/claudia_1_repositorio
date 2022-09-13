efb56d354def4e4412eb18c386579b116c42a481
claudiasantivaniez <santivaniezclaudia@gmail.com>
2022-09-12 22:22:28 -0300

    ejercicio_1

---
+++ modulo1/clase1.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Claudia Santivañez
+#
+# Created:     12/09/2022
+# Copyright:   (c) Claudia Santivañez 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    papa = 1
+    cebolla = 2
+    suma = papa + cebolla
+    print (suma)
+if __name__ == '__main__':
+    main()
