fa6ab0d4bfe76aa99589c5f1c3ef15d13c1b76d6
claudiasantivaniez <santivaniezclaudia@gmail.com>
2022-09-20 18:30:10 -0300

    ejercicio1_final

--- clase2_ejercicio1.py
+++ clase2_ejercicio1.py
@@ -27,30 +27,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-    def main():
-#ejercicio 2
-#Pedir al usuario que ingrese dos nros
-#Luego imprimir 3 opciones (1. sumar, 2. restar y 3. multiplicar)
-#Pedir al usuario que ingrese una opción
-#Verificar la opción del usuario
-#Mientras que el usuario no ingrese una opción correcta se volverá a pedir que
-#ingrese una opción
-#Ejecutar la operación
-#Mostrar por pantalla el resultado
-
-     a = int(input("ingrese un numero "))
-     b = int(input("ingrese un numero "))
-   # print ("1 sumar, 2 restar, 3 multiplicar")
-    operacion = int(input("ingrese una opcion: 1 sumar, 2 restar, 3 multiplicar "))
-    while operacion < 1 or operacion > 3:
-        operacion = int(input("ingrese una opcion valida: 1 sumar, 2 restar, 3 multiplicar "))
-    if (operacion==1):
-        op=(a+b)
-    elif (operacion==2):
-        op=(a-b)
-    else:
-        op=(a*b)
-    print(f'El resultado es: {op}')
-
-if __name__ == '__main__':
-    main()
